fc79aaddadf3c930156eb39bafcb06ed8a4ffbd7
walterjrr <jrwalter731@gmail.com>
2022-01-18 13:44:14 -0300

    criando a interface completa da calculadora

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import React from "react"
+import './Button.css'
+
+const Button = (props) => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    classes += props.equal ? 'equal' : ''
+
+    return (
+        <button
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
+
+export default Button
